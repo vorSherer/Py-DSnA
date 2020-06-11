53a7c6933a1c0240667669804fff2913901a531e
vorSherer <tcpma2011@gmail.com>
2020-06-10 18:36:38 -0700

    Added code and working tests, updated README.md

--- py_dsna/401/challenges/array-binary-search/tests/test_binary_search.py
+++ py_dsna/401/challenges/array-binary-search/tests/test_binary_search.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from array_binary_search.array_binary_search import binary_search
 
 def test_binary_search_exists():
@@ -8,18 +9,42 @@ def test_binary_search_exists():
 
 # @pytest.mark.skip
 def test_binary_search_returns_inputs():
-    expected = [1,2,3,4,5], 2
+    expected = 1
     actual = binary_search([1,2,3,4,5], 2)
     assert actual == expected
 
 
 # @pytest.mark.skip
 def test_binary_search_returns_fail():
-    expected = [1,2,3,4,5], 2
+    expected = -1
     actual = binary_search([1,2,3,4,5], 4)
     assert actual != expected
 
 
-# def test_binary_search
+# @pytest.mark.skip
+def test_binary_search_no_match():
+    expected = -1
+    actual = binary_search([11,22,33,44,55,66,77], 90)
+    assert actual == expected
 
+
+def test_search_100_pass():
+    expected = 43
+    test_list = [i for i in range(100)]
+    actual = binary_search(test_list, 43)
+    assert actual == expected
+
+
+def test_search_10000_pass():
+    expected = 9998
+    test_list2 = [i for i in range(10000)]
+    actual = binary_search(test_list2, 9998)
+    assert actual == expected
+
+# @pytest.mark.skip
+def test_search_100000_pass():
+    expected = 441
+    test_list3 = [i for i in range(100000)]
+    actual = binary_search(test_list3, 441)
+    assert actual == expected
 
