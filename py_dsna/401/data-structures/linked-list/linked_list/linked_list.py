96825f0ede75cbe96b61ffdd116550ce24f1f4b7
vorSherer <tcpma2011@gmail.com>
2020-06-13 16:42:10 -0700

    Initial commit, with commented requirements.

--- py_dsna/401/data-structures/linked-list/linked_list/linked_list.py
+++ py_dsna/401/data-structures/linked-list/linked_list/linked_list.py
@@ -1,94 +1,10 @@
-class LinkedList:
-# ***************************************
-# Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
 
-    def __init__(self):   #, value=None):
-        # self.value = value
-        self.head = None
-
-    def __repr__(self):
-        return f"LinkedList : {self.head}"
-
-# ***************************************
-# Define a method called __str__ which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
-# "{ a } -> { b } -> { c } -> NULL"
-# ****************************************
-
-    def __str__(self):
-        '''
-        { bananas } --> { apples } --> NULL
-        '''
-        response = ""
-
-        current = self.head
-
-        while current:
-            response += f"{{ {str(current.value)} }} -> " 
-            current = current.next_node
-
-        return response + "NULL"
-
-# ***************************************
-# Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
-
-    def insert(self, value):
-
-        self.head = Node(value, self.head)
-
-
-# ***************************************
-# Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-
-    def includes(self, value):
-
-        current = self.head
-
-        while current:
-            if current.value == value:
-                return True 
-            current = current.next_node
-        return False
-
-
-# ***************************************
 # Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-
-
-class Node:
-
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
-
-    def has_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next_node
-
-
-#     # To throw an exception deliberately - test_insert_empty...
-#     # research a better way to write this, would usually do "Duck Typing":
-#     # if not isinstance(next_, Node) and next_ != None:
-#     #   raise TypeError("Next node 
-
-    def __repr__(self):
-        return f"{self.value} : {self.next}"
-
-# if __name__ == "__main__":
-    
-#     apples = Node("apples")
-
-#     bananas = Node("bananas")
-
-#     apples.next = bananas
-
-#     print(apples)
-
-
-    # fruit = LinkedList()
-
-    # fruit.head = apples
-
-    # print(fruit)
+# Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
+# Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+# Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+# Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+# "{ a } -> { b } -> { c } -> NULL"
+# Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
+# Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
 
