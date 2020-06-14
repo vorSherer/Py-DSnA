a74f82670f26f967964d1e4f56370e2c43d29053
vorSherer <tcpma2011@gmail.com>
2020-06-13 17:12:18 -0700

    Added basic Node class.

--- py_dsna/401/data-structures/linked-list/linked_list/linked_list.py
+++ py_dsna/401/data-structures/linked-list/linked_list/linked_list.py
@@ -1,10 +1,28 @@
-
 # Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+class Node:
+    '''
+    This is the Node class. It is used to instantiate a new node for the linked list.
+    '''
+    def __init__(self, value, next_node=None):
+        '''
+        This function initializes the Node class. It takes in a value and an optional value for next_node, pointing to the next node in the list. If no value is supplied for next_node, it will be initialized to "None," indicating that this will be the head node.
+        '''
+        self.value = value
+        self.next_node = next_node
+
+    def __repr__(self):
+        return f"{self.value} : {self.next_node}"
+
 # Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
+
 # Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+
 # Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-# Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+
+# Define a method called __str__ which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
 # "{ a } -> { b } -> { c } -> NULL"
+
 # Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
+
 # Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
 
