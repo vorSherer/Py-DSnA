c827425779e1e992cf864ef6cce085ccfb1ddb34
vorSherer <tcpma2011@gmail.com>
2020-06-13 18:56:54 -0700

    Added LinkedList class and basic assert tests.

--- py_dsna/401/data-structures/linked-list/tests/test_linked_list.py
+++ py_dsna/401/data-structures/linked-list/tests/test_linked_list.py
@@ -1,11 +1,26 @@
+from linked_list.linked_list import (
+    Node,
+    LinkedList,
+)
+
+def test_Node_exists():
+    assert Node
 
 
-# Write tests to prove the following functionality:
+def test_LinkedList_exists():
+    assert LinkedList
+
 
 # Can successfully instantiate an empty linked list
+
 # Can properly insert into the linked list
+
 # The head property will properly point to the first node in the linked list
+
 # Can properly insert multiple nodes into the linked list
+
 # Will return true when finding a value within the linked list that exists
+
 # Will return false when searching for a value in the linked list that does not exist
+
 # Can properly return a collection of all the values that exist in the linked list
