5cc6a846a28d3f0fde433e46dea6668c65f5f6b0
vorSherer <tcpma2011@gmail.com>
2020-07-06 21:55:30 -0700

    Initial commit - basic scaffold, code template and test template (not importing).

---
+++ py_dsna/401/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -0,0 +1,18 @@
+class AnimalShelter:
+    """
+    Docstring here
+    """
+    def __init__(self):
+        self._front = None
+        self._rear = None
+
+    def enqueue(self, animal):
+        pass
+
+    def dequeue(self, pref):
+        pass
+
+
+
+    if __name__ == "__main__":
+        pass
